117c13aa7506bbbfb5f02e38b3463510b591e408
mrglk <mariagalko.info@gmail.com>
2022-03-31 17:24:31 +0300

    Random products for main

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import "./js/search.js";
 
 import { initCatalog } from "./js/catalog";
 import { initProductPage } from "./js/product";
+import { initProductMain } from "./js/mainProducts";
 
 const catalog = document.getElementById('catalog');
 const productPage = document.getElementById('product')
+const mainPage = document.getElementById('main')
 
 if (catalog) {
     initCatalog()
@@ -14,4 +16,8 @@ if (catalog) {
 
 if (productPage) {
     initProductPage()
+}
+
+if (mainPage) {
+    initProductMain()
 }
---
+++ src/js/mainProducts.js
@@ -0,0 +1,37 @@
+import { getProducts } from "./getProducts";
+
+export function initProductMain() {
+
+getProducts().then(data => {
+    randomCardsForMain(data)
+});
+}
+
+function createCardForMain(item) {
+    return ` <div class="ourFavorites__item">
+    <a href="./product.html?id=${item.id}" class="ourFavorites__productLink">
+        <div class="ourFavorites__product">
+            <div class="ourFavorites__imgWrapper">
+                <img class="ourFavorites__img" src="../uploads/${item.image}" alt="Rings">
+            </div>
+            <p class="ourFavorites__name">${item.name}</p>
+            <p class="ourFavorites__price">${item.price}$</p>
+        </div>
+    </a>
+</div>`;
+};
+
+
+function randomCardsForMain(products) {
+    const line1 = document.querySelector(".ourFavorites__line1");
+    const line2 = document.querySelector(".ourFavorites__line2");
+    line1.innerHTML = "";
+    line2.innerHTML = "";
+    const count = products.length;
+
+    for (let i = 0; i < 2; i++) {
+        line1.innerHTML += createCardForMain(products[Math.round(Math.random() * count) + 1]) 
+    }
+
+    line2.innerHTML = createCardForMain(products[Math.round(Math.random() * count) + 1]) 
+};
