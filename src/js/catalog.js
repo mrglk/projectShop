0be3e8c63c302c1706726e37ee8240e2a451f398
mrglk <mariagalko.info@gmail.com>
2022-04-02 18:45:53 +0300

    Added part of pagination

--- src/js/catalog.js
+++ src/js/catalog.js
@@ -4,19 +4,41 @@ import {
 
 const filterLinks = document.querySelectorAll('.catalogHeader__link');
 const urlGroupName = window.location.search.replace( '?group=', '');
+const pagination = document.querySelector(".js-pagination");
 
 export function initCatalog() {
 getProducts().then(data => {
+    const totalProducts = data.length;
+    const productsPerPage = 9;
+    const totalPages = Math.ceil(totalProducts / productsPerPage);
+
+    let currentPage = 1;
+    let offset = 0;
+
+    if (totalProducts > productsPerPage) {
+        const arrowBack = `<li class="pagination__link js-pagi-back"><img class="pagination__arrow js-pagi-back"
+        src="../img/arrowBack.svg">`;
+        const arrowNext = ` <li class="pagination__link js-pagi-next">
+        <img class="pagination__arrow" src="../img/arrowNext.svg">
+    </li>`;
+    let newLinks = "";
+
+    for (let i = 1; i <= totalPages; i++) {
+        newLinks += `<li class="pagination__link js-pagi-number">${i}</li>`;
+    }
+    pagination.innerHTML += arrowBack;
+    pagination.innerHTML += newLinks;
+    pagination.innerHTML += arrowNext;
+      }
 
     if (urlGroupName) {
+        pagination.classList.toggle("pagination__inner_hidden", filterCatalog(urlGroupName, data).length <= productsPerPage); // хуйня
         renderCardsForCatalog(filterCatalog(urlGroupName, data));
                 filterLinks.forEach(link => {
-                    if (link.innerHTML.toLowerCase() == urlGroupName) {
-                        link.classList.add("catalogHeader__link_active");
-                    }
-                })
+                    link.classList.toggle("catalogHeader__link_active", link.innerHTML.toLowerCase() == urlGroupName);
+                })               
     } else {
-        renderCardsForCatalog(data)
+        renderCardsForCatalog(data.slice(offset, offset + 9))
     };
 
     for (let i = 0; i < filterLinks.length; i++) {
@@ -30,6 +52,48 @@ getProducts().then(data => {
         filterLinks[i].classList.add("catalogHeader__link_active");
         });
     };
+
+    pagination.addEventListener("click", function(e) {
+       const element = e.target;
+       const paginationLink = document.querySelectorAll(".pagination__link");
+
+       paginationLink.forEach(link => {
+            link.classList.toggle("pagination__link_active", element == link)
+       });
+
+       if(element.classList.contains("js-pagi-number")) {
+        const newPage = Number(element.innerText)
+        setPage(newPage)
+       }
+
+       if (element.classList.contains('js-pagi-next')) {
+        // Добавить проверки чтобы нельзя было уйти ниже первой страницы и дальше максимальной
+        if (totalPages <= currentPage) {
+            element.classList.add("pagination__link__disabled")
+        } else {
+            setPage(currentPage + 1)
+        }
+    }
+        
+        if (element.classList.contains('js-pagi-back')) {   
+            if (currentPage = 1) {
+                element.classList.add("pagination__link__disabled")
+            } else {
+                setPage(currentPage - 1)
+            }
+    
+        }
+    })
+
+    function setPage(page) {
+        currentPage = page;
+        offset = page * productsPerPage;
+        renderCardsForCatalog(data.slice((currentPage - 1) * productsPerPage, ((currentPage - 1) * productsPerPage) + 9))
+        window.scrollTo(0, 0);
+
+    }
+    
+
 }); }
 
 export function createCardForCatalog(item) {
