dd6d0f3cee169910271b97b68b797de5b400c5dd
AnnaBegisheva <annabegisheva@gmail.com>
2022-04-02 22:33:59 +0300

    subscribe validation added

--- src/js/mainProducts.js
+++ src/js/mainProducts.js
@@ -37,3 +37,34 @@ function randomCardsForMain(products) {
 
     line2.innerHTML = createCardForMain(products[Math.round(Math.random() * count) + 1])
 };
+
+let subscribeButton = document.querySelector('.subscribe__button');
+subscribeButton.onclick = function (e) {
+    e.preventDefault();
+    let emailInput = document.querySelector('.subscribe__formField');
+    let pattern = /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i;
+    document.querySelector('.subscribe__message').innerHTML = "";
+    if (pattern.test(emailInput.value) == true) {
+        document.querySelector('.subscribe__message').innerHTML = "Thank you! You're successfully subscribed";
+        sendForm();
+    } else {
+        document.querySelector('.subscribe__message').innerHTML = "Please provide valid email!"
+    }
+}
+
+let sendForm = () => {
+    let json = {
+        email: document.querySelector('.subscribe__formField'),
+        subscription: true,
+    }
+
+    fetch("https://httpbin.org/post", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(json)
+        })
+        .then(response => console.log(response.json()))
+        .catch(error => console.log(error));
+};
